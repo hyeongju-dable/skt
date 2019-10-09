e619c7028e5c867d76fbad2341178a45f21cbe3b
hyeongju88 <chj8081@gmail.com>
2019-10-10 00:45:01 +0900

    remove unused file

--- run_by_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -
-work_home=$(cd $(dirname ${0}); pwd)
-
-docker image build  -t skt:latest .
-
-docker run -p 9999:9999 -t --name skt --net=host skt:latest
-sleep 5
-docker container rm skt
--- run_by_pull.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -
-work_home=$(cd $(dirname ${0}); pwd)
-
-docker pull chj8081/skt:v1
-docker run -p 9999:9999 -t --name skt --net=host chj8081/skt:v1
-sleep 5
-docker container rm skt
